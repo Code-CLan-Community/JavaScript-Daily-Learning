70ac6166e665e811be1c092a5ad04f4d63936460
Code-Clan-Community <veergupta0801@gmail.com>
2024-05-23 09:01:10 +0530

    JavaScript Learning

---
+++ Day 4/index.js
@@ -0,0 +1,25 @@
+// operators
+// Assigmental 
+// (Binary) Arthmetic --> + - / * % **
+// Shorthand --> += -= /= %= **=
+// (unaray) Inc/dec  ++ --
+
+
+let n1 =6,n2=2, op=null;
+op = n1**n2;
+console.log(op);
+
+op = n1+n2;
+console.log(op);
+
+op = n1-n2;
+console.log(op);
+
+op = n1*n2;
+console.log(op);
+
+op = n1/n2;
+console.log(op);
+
+op = n1%n2;
+console.log(op);
---
+++ day2.js
@@ -0,0 +1,31 @@
+// var/let/const
+// variable name/ contaoner name 
+// assignment opertor
+// value 
+
+// ex - var age = 39
+
+// Naming convention 
+// 1. related to data 
+// 2. start with small / capital letter or "_"
+// 3. not start with numerical throught can be user after first letter 
+
+// 4 . must be a single word 
+
+// 5. use seperetors - Case patterns--> snake_case_pattern
+                        //camel Case -> camelCasePattern
+                        // Pascal Case --> PascalCasePattern
+
+
+// DataType
+
+// number 123/-123/-1.2/-1.3
+// string - '123'/"asc"/"@%#$$"
+// Boolean -> true,false 
+// null (Object) --> when programmer intentionally empty the variable 
+// undefined - when complier wants to show the empty value.
+
+
+
+             
+
