ccf420de84697b8c9478815092a5fd143eda4817
Code-Clan-Community <veergupta0801@gmail.com>
2024-06-05 09:24:38 +0530

    loops

---
+++ Day 7/loops.js
@@ -0,0 +1,69 @@
+// Iterative Statments--
+
+// Pillars of Loop: 
+
+// 1. Loop Variable  --> holds the count of iteration(let =1)
+// 2. Condition Expression -> holds the condition for iterating the loop(i<=10)
+// 3. Updation Expression --> it increments and deceremnts the value of loop variable.
+// 4. Loop body -> contains the lines of which are executing. (c.l.g("any statement")).
+
+// Types of Loops :
+// 1. Entry Control Loop --> for loop, while loop.
+// 2. Exit Control Loop --> do while loop.
+
+
+//WAP to print n natural numbers(i -n):
+
+
+// -------------------------------------------------------------------------------------------------------
+//while loop syntax --------
+
+
+// let loop_vaariable = value;
+
+// while( condition_expression ){
+    // body of loop 
+    // updaation expression
+// }
+
+
+// let i = 1;
+
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+
+// -------------------------------------------------------------------------------------------------------
+// for loop syntax 
+
+
+// for(let loop_variable= value; conditionn_expression; updation_expression)
+    // {
+        // body of loop
+    // }
+
+// for (let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// -------------------------------------------------------------------------------------------------------
+//do while loop syntax
+
+
+// let loop_vaariable = value;
+
+// do{
+    // body of loop 
+    // updaation expression
+// }while(condition_expression);
+
+
+// do{
+//      console.log(i);
+//      i++;  
+// }while(i<=10);
+
+
+
