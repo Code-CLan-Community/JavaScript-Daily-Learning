ff48391b0e04416c8c57427e4575951f36afaf69
Code-Clan-Community <veergupta0801@gmail.com>
2024-06-06 18:39:31 +0530

    Day 8 practice programs

--- Day 8/ques.js
+++ Day 8/ques.js
@@ -1,20 +1,20 @@
 // Wap a pg to 
 //  let n = prompt("Enter any number"); //Number()
-// console.log(n);
-// console.log(typeof n);
+// alert(n);
+// alert(typeof n);
 
 // Wap to accept Name of the User and print "Given name is Jhon"
 
 // let k = prompt("enter your name");
-// console.log("Given name is "+ k);
+// alert("Given name is "+ k);
 // // Template strings
-// console.log(`Username is ${k}`);
+// alert(`Username is ${k}`);
 
 //Wap a pg to accept two number and print thier sum
 
 // let a = +prompt("num1"), b=+prompt("num2");
 
-// console.log(`10 + 20 = ${a+b}`);
+// alert(`10 + 20 = ${a+b}`);
 
 // Wap to accpet name and 3 diffrent subject marks from the end user and calculate total percentage of it.
 
@@ -32,9 +32,9 @@
 //Wap a p.g to convert F to celcius?
 
 // let t1 = +prompt("Enter Temperature in F");
-// console.log(`Temperature in Celcius is ${F-32}`);
+// alert(`Temperature in Celcius is ${F-32}`);
 
-//wap to calculate the simple intrest ?
+// wap to calculate the simple intrest ?
 
 // let p = +prompt("Principal");
 // let r = +prompt("Rate");
@@ -42,8 +42,25 @@
 
 // let Si = (p*r*t)/100;
 
-// alert(`Simple Intrest will be ${Si}`);
+// alert(`Simple Intrest will be ${Si.toFixed(2)}`);
 
-//Wap to accept Name, Gender and age of the person and check westher he/she is abale to vote or not.
+// Wap to accept Name, Gender and age of the person and check westher he/she is abale to vote or not.
 // output : congrulations Mr. ABC you can vote
-//failure output: Oh sorry Mrs. Abc can not vote
+// failure output: Oh sorry Mrs. Abc can not vote
+
+// let Name = prompt("Enter Your Name");
+// let Gen = prompt("Enter your Gender (M/F) ");
+// let age = +prompt("Enter your age in digits ");
+
+// if(age>=18){
+//     if(Gen=="M")
+//         alert(`Congrulations Mr. ${Name} are able to vote`);
+//     else
+//         alert(`Congrulations Mrs. ${Name} are able to vote`);
+// }
+// else{
+//     if(Gen=="M")
+//         alert(`Oops!, Mr. ${Name} are not able to vote`);
+//     else
+//         alert(`Oops!, Mrs. ${Name} are not able to vote`);
+// }
