dafbf9e02080fe829489d8beda0a2404632a74de
Code-Clan-Community <veergupta0801@gmail.com>
2024-06-12 09:10:24 +0530

    QUES

---
+++ Day 10/string.js
@@ -0,0 +1,39 @@
+// DAy 46: Strings - Escappse sequences, 
+
+
+// indexing always start from zero.
+
+let s =  "Hello World!";
+
+// s = hell world;
+// console.log(s);
+// console.log(s[0]);
+// console.log(s.at(s[-1]));
+// s = null;
+
+// /String Templates 
+
+// console.log("given string "+ s);
+// console.log(`given string ${s}`);
+
+//Escape Sequence Character in strings
+
+// \n \t \b \r   \(escape sequence)
+
+// console.log("Hello\nworld");
+// console.log("hello\tworld");
+
+// console.log("http:\\127.0.0.1:5500\new\tab");
+
+// console.log(`http:\\127.0.0.1:5500\new\tab`);
+
+// escape sequence character
+// console.log("http:\\\127.0.0.1:5500\\new\\tab");
+
+//console.log(String.raw`http:\\127.0.0.1:5500\new\tab`);
+
+
+
+
+
+
