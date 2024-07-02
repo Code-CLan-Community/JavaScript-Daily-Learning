88ddfff01173af9c3c9550f3b6e000ed1d5f1157
Code-Clan-Community <veergupta0801@gmail.com>
2024-07-02 23:48:29 +0530

    day 21 - dom

---
+++ Day 21/Dom.js
@@ -0,0 +1,24 @@
+// console.log(document);
+
+// document.getElementsByTagName("h1");
+// document.getElementsByClassName("class");
+// document.getElementById("id");
+
+// const heading1 = document.querySelector("h1"); // Single Starting tag
+// const heading2 = document.querySelectorAll("h1");//for multiple tag
+
+// console.log(heading1);
+// console.log(heading2[1]);
+
+// console.log(heading1.textContent);
+// console.log(heading1.innerText);
+// // mostly using by developers
+// // aquire the properties of html
+// console.log(heading1.innerHTML);
+
+// heading1.innerHTML = "Changing The content";
+// console.log(heading1);
+
+body.style.backgroundColor = "red";
+// heading1.style.color ="red";
+// heading1.style.font ="serif";
